f440b0646ee32169fa79db786bdd3fdab9a2e4ac
Nathaniel Schier <nathaniel.schier@gmail.com>
2017-10-04 00:58:17 -0700

    Add base dependencies, initial React component, and webpack config.

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        MAIN PAGE
+      </div>
+    )
+  }
+}
+
+module.exports = App;
