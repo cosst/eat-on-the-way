948d403df3284cef4ffb352f08b2ef873c948835
Nathaniel Schier <nathaniel.schier@gmail.com>
2017-10-23 11:24:05 -0700

    Update API so that Yelp results filter by nearest distance.

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,11 @@
 var axios = require('axios');
 
+function sortBusinesses (players) {
+  return players.sort(function (a,b) {
+    return b.score - a.score;
+  });
+}
+
 module.exports = {
   getDriveTime: function () {
     return axios.get('/drive-time')
@@ -15,7 +21,12 @@ module.exports = {
     return axios.get('/cafes', {params: {address: address}})
     .then(function (response) {
       console.log(response.data.jsonBody.businesses);
-      return response.data.jsonBody.businesses;
+      var businesses = response.data.jsonBody.businesses
+      .sort(function(a, b) {
+        return a.distance - b.distance;
+      })
+      return businesses;
+      console.log(businesses);
     })
     .catch(function (error) {
       console.log(error);
