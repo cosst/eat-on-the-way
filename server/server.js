95b09348be6cc23c4c6213b7eec46dd8c675e37b
Nathaniel Schier <nathaniel.schier@gmail.com>
2017-11-30 09:00:41 -0800

    Add webpack-dotenv-plugin to dependencies from dev.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import open from 'open';
 import routes from './routes';
-// require('dotenv').config();
 
-// const port = 3000;
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
