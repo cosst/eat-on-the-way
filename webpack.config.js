c0880c4be72f4ec4fe44de94f851b435ff31e908
Nathaniel Schier <nathaniel.schier@gmail.com>
2017-11-30 08:38:19 -0800

    Update webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -37,14 +37,14 @@ var config = {
     })]
 }
 
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    })
-  )
-}
+// if (process.env.NODE_ENV === 'production') {
+//   config.plugins.push(
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+//       }
+//     })
+//   )
+// }
 
 module.exports = config;
